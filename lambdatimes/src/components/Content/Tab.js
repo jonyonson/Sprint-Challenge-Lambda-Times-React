cb230551beba542b8ff00bbc1bb1f9984aa50c5b
Jonathan Taylor <jonyonson@icloud.com>
2019-05-03 11:12:02 -0500

    fixed Tabs, Tab, Cards, Card - can now filter cards with tabs

--- lambdatimes/src/components/Content/Tab.js
+++ lambdatimes/src/components/Content/Tab.js
@@ -1,16 +1,13 @@
 import React from 'react';
 
 const Tab = props => {
-  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop, 
-      if they match, the className should be: 'tab active-tab', 
+  /* Using your props, determine if the `tab` prop matches the `selectedTab` prop,
+      if they match, the className should be: 'tab active-tab',
       if it is not it should just be 'tab'*/
   return (
     <div
-      className={''}
-      onClick={() => {
-        /* Replace this dummy click handler function with your selectTabHandler function from props 
-         you'll need to pass the `tab` in as an argument to this handler. */
-      }}
+      className={props.tab === props.selectedTab ? 'tab active-tab' : 'tab'}
+      onClick={() => props.selectTabHandler(props.tab)}
     >
       {props.tab.toUpperCase()}
     </div>
@@ -20,3 +17,9 @@ const Tab = props => {
 // Make sure you include PropTypes on your props.
 
 export default Tab;
+
+/* <div
+className={''}
+onClick={() => {
+  /* Replace this dummy click handler function with your selectTabHandler function from props
+   you'll need to pass the `tab` in as an argument to this handler. */
